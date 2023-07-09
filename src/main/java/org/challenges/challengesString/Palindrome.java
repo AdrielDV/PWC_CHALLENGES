1b0a00f1c3687946de79282ae5f01f1bd61d6e87
AdrielDV <adrielnds90@gmail.com>
2023-07-09 17:05:08 -0300

    Correção de restrição de input.

--- src/main/java/org/challenges/challengesString/Palindrome.java
+++ src/main/java/org/challenges/challengesString/Palindrome.java
@@ -17,7 +17,7 @@ public class Palindrome {
         return IntStream.range(0, input.length())
                 .boxed()
                 .flatMap(i -> IntStream.rangeClosed(i + 1, input.length()).mapToObj(j -> input.substring(i, j)))
-                .filter(substring -> substring.length() > 1)
+                .filter(substring -> substring.length() > 0)
                 .filter(substring -> new StringBuilder(substring).reverse().toString().equals(substring))
                 .max(Comparator.comparingInt(String::length))
                 .orElse("Não tem palíndromo");
